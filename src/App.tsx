92ba8fb5352bb26854deaf5216b0ae21ba4b1e30
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-27 20:24:57 +0200

    feat: implement todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,79 @@
-import TodoList from "./components/TodoList"
-import TodoListElement from "./components/TodoListElement"
-import Input from "./components/Input"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+
+interface Todo { label: string, checked: boolean }
 
 function App() {
+  const [inputValue, setInputValue] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+  
+  useEffect(() => {
+    const savedTodos = localStorage.getItem('todos')
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos)) 
+    }
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    if (!loading) {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }
+  }, [loading, todos])
+
+  const onInputValueChange = (evt: ChangeEvent<HTMLInputElement>) => setInputValue(evt.target.value)
+  
+  const onFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    const form = evt.target as HTMLFormElement
+    const input = form[0] as HTMLInputElement
+    const inputValue = input.value.trim()
+    
+    if(inputValue.length >= 1) {
+      setTodos([...todos, {label: inputValue, checked: false}])
+      setInputValue('')
+    }
+  }
+  
+  const onTodoCheck = (id: number) => {
+    const updatedTodos = todos
+    updatedTodos[id].checked = !updatedTodos[id].checked
+    setTodos([...updatedTodos])
+  }
+
+  const onTodoRemove = (id: number) => {
+    setTodos(todos?.filter((_, index) => id !== index))
+  }
+
   return (
     <>
-      <h1>todo app</h1>
-      <Input />
-      <TodoList />
-      <TodoListElement />
+      {loading ? <p>loading</p> : (
+        <>
+        <h1>todo app</h1>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" value={inputValue} onChange={onInputValueChange} />
+          <button type="submit">add</button>
+        </form>
+
+        {todos && todos.length >= 1 ? (
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>
+                <label>
+                  <input
+                    type="checkbox"
+                    id={`${index}_${todo.label}`}
+                    checked={todo.checked}
+                    onChange={() => onTodoCheck(index)}
+                  />
+                  <span>{todo.label}</span>
+                </label>
+                <button onClick={() => onTodoRemove(index)}>x</button>
+              </li>))}
+          </ul>
+        ) : <p>no todos</p>}
+      </>
+      )}
     </>
   )
 }
