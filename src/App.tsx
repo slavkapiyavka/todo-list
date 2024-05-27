3ac8876d2509096210fafaddc06842065a871be9
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-28 00:53:24 +0200

    refactor: create state for disabledSubmitButton. add PlaceholderText, set styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import PlaceholderText from "./PlaceholderTextComponent"
 
-interface Todo { label: string, checked: boolean }
+interface Todo { title: string, checked: boolean }
 
 function App() {
   const [inputValue, setInputValue] = useState<string>('')
   const [todos, setTodos] = useState<Todo[]>([])
+  const [disabledSubmitButton, setDisabledSubmitButton] = useState(!inputValue)
   const [loading, setLoading] = useState<boolean>(true)
   
   useEffect(() => {
@@ -21,7 +23,11 @@ function App() {
     }
   }, [loading, todos])
 
-  const onInputValueChange = (evt: ChangeEvent<HTMLInputElement>) => setInputValue(evt.target.value)
+  const onInputValueChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setDisabledSubmitButton(!evt.target.value)
+    
+    setInputValue(evt.target.value)
+  }
   
   const onFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault()
@@ -30,8 +36,9 @@ function App() {
     const inputValue = input.value.trim()
     
     if(inputValue.length >= 1) {
-      setTodos([...todos, {label: inputValue, checked: false}])
+      setTodos([...todos, { title: inputValue, checked: false}])
       setInputValue('')
+      setDisabledSubmitButton(true)
     }
   }
   
@@ -46,35 +53,47 @@ function App() {
   }
 
   return (
-    <>
-      {loading ? <p>loading</p> : (
-        <>
-        <h1>todo app</h1>
-        <form onSubmit={onFormSubmit}>
-          <input type="text" value={inputValue} onChange={onInputValueChange} />
-          <button type="submit">add</button>
-        </form>
+    <section className="app">
+      <h1 className="app__title">todo app</h1>
+      {
+        loading ?
+        <PlaceholderText label="loading" />
+        :
+        (
+          <>
+              <form onSubmit={onFormSubmit} className="todo-form">
+                <input className="todo-form__input" type="text" value={inputValue} onChange={onInputValueChange} />
+                <button type="submit" disabled={disabledSubmitButton}>Add</button>
+              </form>
 
-        {todos && todos.length >= 1 ? (
-          <ul>
-            {todos.map((todo, index) => (
-              <li key={index}>
-                <label>
-                  <input
-                    type="checkbox"
-                    id={`${index}_${todo.label}`}
-                    checked={todo.checked}
-                    onChange={() => onTodoCheck(index)}
-                  />
-                  <span>{todo.label}</span>
-                </label>
-                <button onClick={() => onTodoRemove(index)}>x</button>
-              </li>))}
-          </ul>
-        ) : <p>no todos</p>}
-      </>
-      )}
-    </>
+              {todos.length <= 0 ?
+              <PlaceholderText label="no todos" />
+              :
+              <ul className="todo-list">
+                {todos.map((todo, index) => (
+                  <li key={index} className="todo-element">
+                    <label className="todo-element__label">
+                      <input
+                        className="todo-element__checkbox"
+                        type="checkbox"
+                        id={`${index}_${todo.title}`}
+                        checked={todo.checked}
+                        onChange={() => onTodoCheck(index)}
+                      />
+                      <span className="todo-element__title">{todo.title}</span>
+                    </label>
+                    <button onClick={() => onTodoRemove(index)} className="todo-element__remove-button">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="${iconName}-icon">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      </svg>
+                    </button>
+                  </li>))}
+              </ul>
+              }
+            </>
+        )
+      }
+    </section>
   )
 }
 
