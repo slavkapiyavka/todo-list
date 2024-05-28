0a4b9970eb66123b9f0c057f2bc48323723a86bc
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-05-28 20:05:42 +0200

    feat: implement todo edit mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,14 @@ import PlaceholderText from "./PlaceholderTextComponent"
 interface Todo { title: string, checked: boolean }
 
 function App() {
+  const editTodoInputRef = useRef<HTMLInputElement>(null)
   const todoInputRef = useRef<HTMLInputElement>(null)
   const [inputValue, setInputValue] = useState<string>('')
   const [todos, setTodos] = useState<Todo[]>([])
   const [disabledSubmitButton, setDisabledSubmitButton] = useState(!inputValue)
   const [loading, setLoading] = useState<boolean>(true)
-  
-  useEffect(() => {
-    const savedTodos = localStorage.getItem('todos')
-    if (savedTodos) {
-      setTodos(JSON.parse(savedTodos)) 
-    }
-    setLoading(false)
-
-    if (todoInputRef.current) {   
-      todoInputRef.current.focus()
-    }
-  }, [loading])
-
-  useEffect(() => {
-    if (!loading) {
-      localStorage.setItem('todos', JSON.stringify(todos))
-    }
-  }, [loading, todos])
+  const [editTodoIndex, setEditTodoIndex] = useState<number | null>(null)
+  const [editTodoValue, setEditTodoValue] = useState('')
 
   const onInputValueChange = (evt: ChangeEvent<HTMLInputElement>) => {
     setDisabledSubmitButton(!evt.target.value)
@@ -53,9 +38,55 @@ function App() {
     setTodos([...updatedTodos])
   }
 
+  const onTodoEditStart = (id: number) => {
+    setEditTodoIndex(id)
+    setEditTodoValue(todos[id].title)
+  }
+
+  const onTodoEditEnd = (e) => {
+    if (e.target.value) {
+      const updatedTodos = todos
+      if (editTodoIndex) {
+        updatedTodos[editTodoIndex].title = e.target.value
+      }
+      setTodos([...updatedTodos])
+      setEditTodoIndex(null)
+      setEditTodoValue('')
+    }
+  }
+
   const onTodoRemove = (id: number) => {
     setTodos(todos?.filter((_, index) => id !== index))
   }
+  
+  useEffect(() => {
+    const savedTodos = localStorage.getItem('todos')
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos)) 
+    }
+    setLoading(false)
+
+    if (todoInputRef.current) {   
+      todoInputRef.current.focus()
+    }
+  }, [loading])
+
+  useEffect(() => {
+    if (!loading) {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }
+  }, [loading, todos])
+
+  useEffect(() => {
+    setEditTodoIndex(null)
+    setEditTodoValue('')
+  }, [todos])
+
+  useEffect(() => {
+    if (editTodoInputRef.current) {
+      editTodoInputRef.current.focus()
+    }
+  }, [editTodoIndex])
 
   return (
     <section className="app">
@@ -82,7 +113,7 @@ function App() {
               :
               <ul className="todo-list">
                 {todos.map((todo, index) => (
-                  <li key={index} className="todo-element">
+                  <li key={index} className={`todo-element ${editTodoIndex === index ? 'todo-element_edit-mode' : ''}`}>
                     <label className="todo-element__label">
                       <input
                         className="todo-element__checkbox"
@@ -92,10 +123,28 @@ function App() {
                         onChange={() => onTodoCheck(index)}
                       />
                       <div className="todo-element__pseudo-checkbox"></div>
-                      <span className="todo-element__title">{todo.title}</span>
+                      {
+                        editTodoIndex === index ?
+                        <input
+                          className="todo-element__title todo-element__title_edit-mode"
+                          value={editTodoValue}
+                          onChange={(e) => setEditTodoValue(e.target.value)}
+                          onBlur={(e) => onTodoEditEnd(e)}
+                          ref={editTodoInputRef}
+                        />
+                        :
+                        <span className="todo-element__title">{todo.title}</span>
+                      }
                     </label>
-                    <button onClick={() => onTodoRemove(index)} className="todo-element__remove-button">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="24" height="24">
+                    
+                    <button onClick={() => onTodoEditStart(index)} className="todo-element__edit-button" type="button">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="18" height="18">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                      </svg>
+                    </button>
+
+                    <button onClick={() => onTodoRemove(index)} className="todo-element__remove-button" type="button" disabled={editTodoIndex === index}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="18" height="18">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                       </svg>
                     </button>
